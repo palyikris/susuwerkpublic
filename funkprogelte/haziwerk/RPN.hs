ebe27c6f4717e00361e65ba016d09e76a193f2a7
palyikris <83282307+palyikris@users.noreply.github.com>
2023-12-09 14:23:38 +0100

    postfix hw done

--- funkprogelte/haziwerk/RPN.hs
+++ funkprogelte/haziwerk/RPN.hs
@@ -7,14 +7,20 @@ data Postfix = Constant Integer (Maybe Postfix) | Function (Integer -> Integer -
 
 
 evaluatePostfix :: Postfix -> Maybe Integer
-evaluatePostfix input = helper input [] where
-    helper (Constant x next) [] 
-        | isNothing next = Just x
-        | otherwise = Nothing
-        where 
-                isNothing Nothing = True
-                isNothing _ = False
-    helper (Constant x next) s = helper (next) (x:s)
-    helper (Function f next) (x:y:stack) = helper (maybe (Constant (f y x) Nothing) (Constant (f y x)) (Just next)) stack
-    helper _ _ = Nothing
+evaluatePostfix list = helper list []
+    where
+        -- task: cases w constants
+        helper (Constant el Nothing) [] = Just el
+        -- prompt: if there is no next, and no stack, then it is the result --> Just el
+        helper (Constant el Nothing) stack = Nothing
+        -- prompt: if there is no next, but there is a stack, then it is an error --> Nothing
+        helper (Constant el (Just next)) stack = helper next (el:stack)
+        -- prompt: if there is a next, then push the element to the stack and go on
 
+        -- task: cases w functions
+        helper (Function op Nothing) [x,y] = Just (op y x)
+        -- prompt: if there is no next, but there are two els in stack, do operation then return the result
+        helper (Function op (Just next)) (x:y:stack) = helper (Constant (op y x) (Just next)) stack
+        -- prompt: if there is a next, then operate on the first two els in stack and continue
+        helper _ _ = Nothing
+        -- prompt: if none of the above --> must be an error --> Nothing
