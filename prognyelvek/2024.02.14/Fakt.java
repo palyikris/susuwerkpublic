19b0f7f57c8729a753162259e338e611a867a098
Kribbany <palyi.kristof@gmail.com>
2024-02-14 11:46:40 +0100

    faktorialis rekurzivan

--- prognyelvek/2024.02.14/Fakt.java
+++ prognyelvek/2024.02.14/Fakt.java
@@ -1,15 +1,32 @@
 // task: Írjuk meg az n faktoriálisát kiszámoló programot.
 
 public class Fakt {
+
+    static int fact_rec(int num) {
+
+        if (num < 0) {
+            System.out.println("Negatív!!");
+            return -1;
+        }
+
+        if (num == 1 || num == 0) {
+            return 1;
+        }
+
+        return num * fact_rec(num - 1);
+    }
+
     public static void main(String[] args) {
         System.out.println("Adj egy szamot!");
         int a = Integer.parseInt(System.console().readLine());
 
-        int prod = 1;
+        // int prod = 1;
 
-        for (int i = 1; i < a + 1; i++) {
-            prod *= i;
-        }
+        // for (int i = 1; i < a + 1; i++) {
+        // prod *= i;
+        // }
+
+        int prod = fact_rec(a);
 
         System.out.println("A(z) " + a + " faktorialisa: " + prod);
 
